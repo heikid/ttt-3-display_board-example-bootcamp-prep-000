a428b36a6ffe38e3bcc4a6269056314a1be4ffad
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-10-08 21:17:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cells = "   "
-  rows = [cells, "|", cells, "|", cells]
-  board = [rows, "-----------", rows, "-----------", rows]
-  return board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
